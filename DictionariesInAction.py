2f41c247ce1f5100766d1110c7f9e6b040bf6f7d
Mario Armando Lopez <mario.lopez2@stu.jefferson.kyschools.us>
2018-09-24 10:14:33 -0400

    Final Version of Project

--- DictionariesInAction.py
+++ DictionariesInAction.py
@@ -1,3 +1,4 @@
+""" Mario Lopez, 09/24/2018, DictionariesInAction(Project 2) """
 print("Please Input Your Fantasy Soccer Team Using A 4-4-2 Formation")
 soccer_team = {
     'fs_1': "",
@@ -12,9 +13,31 @@ soccer_team = {
     'def_4': "",
     'gk_1': "",
     }
-for position,player in soccer_team.items():
-    print(position,player)
-    playerInput = input()
-    soccer_team[position] = playerInput
-    print(soccer_team)
 
+def substitutions(position):
+    x = input(" Who would you like to substitute? ")
+    soccer_team[position] = x
+
+def print_team():
+    for k,v in soccer_team.items():
+        print(k,v)
+
+def update_team():
+    pos = input("What position would you like to substitute?")
+    substitutions(pos)
+    print_team()
+
+for position,player in soccer_team.items():
+        print(position,player)
+        playerInput = input()
+        soccer_team[position] = playerInput
+
+while True: 
+    i = input(" Would You Like To Keep This As Your Permanent Team? [y] or [n]")
+    if i == "y" or i == "Y":
+        print(soccer_team)
+        break
+    else:
+        update_team()
+     
+    
